9d9d93986a2ec1fb528beafd1b2b73ec0a3001fa
lexmin0412 <zhangle_dev@outlook.com>
2024-09-07 22:22:29 +0800

    feat: 集成 Ant Design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
